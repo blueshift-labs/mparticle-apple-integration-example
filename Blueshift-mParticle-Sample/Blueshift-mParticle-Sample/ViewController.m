e6e5d9305fc3b1b36b777f0181e7ebb8bfbb14b7
Rahul Raveendran V P <rahul.pmna@gmail.com>
2020-05-01 13:08:47 +0530

    sample app with empty page added

---
+++ Blueshift-mParticle-Sample/Blueshift-mParticle-Sample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Blueshift-mParticle-Sample
+//
+//  Created by Rahul Raveendran on 01/05/20.
+//  Copyright © 2020 Blueshift Labs Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
