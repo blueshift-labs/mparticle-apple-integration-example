9d4f3c150917fdac3d62dbd9f911db37f1aa4ec2
Rahul Raveendran V P <rahul.pmna@gmail.com>
2020-05-10 18:13:59 +0530

    added mp init code and removed scenedelegate

--- Blueshift-mParticle-Sample/Blueshift-mParticle-Sample/ViewController.m
+++ Blueshift-mParticle-Sample/Blueshift-mParticle-Sample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MPKitBlueshift.h>
 
 @interface ViewController ()
 
@@ -16,8 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [MPKitBlueshift registerForInAppMessage:NSStringFromClass([ViewController class])];
 }
 
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [MPKitBlueshift unregisterForInAppMessage];
+}
 
 @end
